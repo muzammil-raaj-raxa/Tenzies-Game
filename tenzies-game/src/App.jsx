1549d3a3c1c085f274b29b6a863610e148f77f5b
Raaj Raxa <muzammilraj0001@gmail.com>
2023-07-31 11:10:49 +0500

    first

---
+++ tenzies-game/src/App.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Die from './Die'
+import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
+import clapSound from './assets/applause.mp3';
+
+
+export default function App() {
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  const audioClap = new Audio(clapSound);
+  audioClap.loop = true;
+
+
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      audioClap.play();
+      console.log('won')
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+          die : generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } : die
+    }))
+  }
+
+  const diceElements = dice.map((
+    die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />
+  ))
+
+  return (
+    <div>
+      <main>
+        {tenzies && <Confetti />}
+
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className='dice-container'>
+          {diceElements}
+        </div>
+        <button
+          className='roll-dice'
+          onClick={rollDice}
+        >
+          {tenzies ? "New Game" : "Roll"}
+        </button>
+      </main>
+      <footer>Developed by <em>Raaj Raxa</em> </footer>
+    </div>
+
+  )
+}
---
+++ tenzies-game/src/Die.jsx
@@ -0,0 +1,19 @@
+
+import React from "react"
+
+export default function Die(props) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
+    return (
+        <div
+            className="dice-box"
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2 className="dice-number">{props.value}</h2>
+        </div>
+    )
+}
